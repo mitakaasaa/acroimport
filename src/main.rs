061367a4c49d4ff65223aaa0cee6684c46d9803c
mitakaasaa <denisstamat69@gmail.com>
2026-05-09 20:36:52 +0200

    Just maked code prettier

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,25 @@ mod exporter;
 mod importer;
 mod models;
 mod pdf;
+mod utils;
 
 use crate::exporter::export::run_export;
 use crate::importer::import::run_import;
+use crate::utils::outputs::{write_help, write_incorrect_call, write_team};
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 2 {
-        println!("Fehlerhafter Programmaufruf!");
-        println!("Verwendung: hundeschule.exe /n | /imp | /exp");
-        return Ok(());
+        return write_incorrect_call();
     }
 
     match args[1].as_str() {
-        "/n" => println!("Denys Stamat - Kate Lagutin - Malcolm Apaloo"),
+        "/n" => write_team()?,
         "/imp" => run_import()?,
         "/exp" => run_export()?,
-        _ => {
-            println!("Fehlerhafter Programmaufruf!");
-            println!("Verwendung: hundeschule.exe /n | /imp | /exp");
-        }
+        "/help" => write_help()?,
+        _ => write_incorrect_call()?,
     }
 
     Ok(())
